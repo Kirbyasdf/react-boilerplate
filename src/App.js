6232bfca94adec2a3d738c8b531547fbe379a869
Augustus Kirby <47834311+Kirbyasdf@users.noreply.github.com>
2021-02-12 05:01:04 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import "tachyons"
 function App() {
   return (
     <div className="bg-black vh-100 white pa3"> 
-    <span className="f1 ">Welcome</span>
-    <div className="f2 mv2">This app has been preloaded with <a className="link hover-dark-red  i " href="https://tachyons.io/" target="_blank">Tachyons</a></div>
-    <a className="link hover-dark-red  f2" href="https://augustuskirby.com/" target="_blank">home</a>
+      <span className="f1 ">Welcome</span>
+        <div className="f2 mv2">This app has been preloaded with
+           <a className="link hover-dark-red  i " href="https://tachyons.io/" target="_blank" rel="noopener noreferrer">
+          Tachyons
+           </a>
+       </div>
+         <a className="link hover-dark-red  f2" href="https://augustuskirby.com/" target="_blank" rel="noopener noreferrer" >
+        home
+         </a>
     </div>
   );
 }
