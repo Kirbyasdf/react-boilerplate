e8622d949129af70062c17eee74a4ea944908f96
kirbyasdf <kirbyaugustus@gmail.com>
2021-02-13 04:57:36 -0500

    version build info added

--- src/App.js
+++ src/App.js
@@ -16,19 +16,31 @@ const HomePage = () => {
 	return (
 		<div className="bg-black vh-100 white pa3 tc">
 			<span className="f1 ">Welcome</span>
-			<br />
-			<a
-				href="https://github.com/Kirbyasdf/react-boilerplate"
-				className="  link "
-				target="_blank"
-				rel="noopener noreferrer"
-			>
-				<img
-					src={gh}
-					className="br4    w2 rotate-vert-center"
-					alt="resource failed to load, f in the console"
-				/>
-			</a>
+			<div className=" f6">
+				current build :{" "}
+				<a
+					className="link  f5"
+					href="https://github.com/facebook/react/blob/master/CHANGELOG.md#1701-october-22-2020"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<span className="blue hover-dark-red ">17.0.1</span>
+				</a>
+			</div>
+			<div className="mt2">
+				<a
+					href="https://github.com/Kirbyasdf/react-boilerplate"
+					className="  link "
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					<img
+						src={gh}
+						className="br4    w2 rotate-vert-center"
+						alt="resource failed to load, f in the console"
+					/>
+				</a>
+			</div>
 			<div className="f3 mv2">
 				<span> This app has been preloaded with</span>
 				<br />
